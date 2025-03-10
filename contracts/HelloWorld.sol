d610a4fa36b8d033c6bb17fc3da4251c21d36d68
Ntarekp <kpntare@gmail.com>
2025-03-10 21:27:04 +0200

    Codes for HelloWorld and Calculator in solidity

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string private message = "Hello, World!";
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
